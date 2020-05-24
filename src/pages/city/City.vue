ba3bf4cfaef42b8848074ec166f56118799f0df4
雪中无处寻 <1179817689@qq.com>
2020-05-24 19:28:42 +0800

    城市列表初步改造封装复用性

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,17 +1,19 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search :cities="cities"></city-search>
-    <city-list :letter="letter" :cities="cities" :hotCities="hotCities"></city-list>
-    <city-alphabet @change="handleLetterChange" :cities="cities"></city-alphabet>
+    <city-search :cities="data.cities"></city-search>
+    <city-list :letter="letter" :cities="data.cities" :hotCities="data.hotCities"></city-list>
+    <city-alphabet @change="handleLetterChange" :cities="data.cities"></city-alphabet>
   </div>
 </template>
 <script>
+import { reactive, onMounted, ref } from 'vue'
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
 import CityList from './components/List'
 import CityAlphabet from './components/Alphabet'
 import axios from 'axios'
+
 export default {
   components: {
     CityHeader,
@@ -19,28 +21,43 @@ export default {
     CityList,
     CityAlphabet
   },
-  data () {
-    return {
-      cities: {},
-      hotCities: [],
-      letter: ''
-    }
-  },
-  created () {
-    this.getCityInfo()
-  },
-  methods: {
-    async getCityInfo () {
-      const { data: res } = await axios.get('/api/city.json')
-      if (!res.ret) return
-      this.cities = res.data.cities
-      this.hotCities = res.data.hotCities
-    },
-    // 获取字母组件传过来的值
-    handleLetterChange (letter) {
-      this.letter = letter
+  setup() {
+    const { data } = useCityLogic()
+    const { letter, handleLetterChange } = useLetterLogic()
+    return { 
+      data,
+      handleLetterChange,
+      letter
     }
   }
+}
+
+function useCityLogic () {
+  const data = reactive({
+    cities: {},
+    hotCities: [],
+  })
+  async function getCityInfo() {
+    const { data: res } = await axios.get('/api/city.json')
+    if (!res.ret) return
+    data.cities = res.data.cities
+    data.hotCities = res.data.hotCities
+  }
+
+  onMounted(() => {
+    getCityInfo()
+  })
+  return { data }
+}
+
+function useLetterLogic () {
+  let letter = ref('')
+  // 获取字母组件传过来的值
+  function handleLetterChange(selected) {
+    letter.value = letter
+  }
+  return { letter, handleLetterChange }
+
 }
 </script>
 <style lang="stylus" scoped>
